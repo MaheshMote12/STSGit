f1eb027471ccc650dbaf2731c05ba9ce7b1e301b
Mahesh <maheshmote12@gmail.com>
2017-12-16 18:15:26 +0530

    added

--- GitAndGithub/src/main/java/com/me/New.java
+++ GitAndGithub/src/main/java/com/me/New.java
@@ -4,11 +4,15 @@ import java.util.Collection;
 import java.util.List;
 
 public class New {
-public static void main(String[] args) {
+public   void main( ) {
 	
 	List l;
 	
-	Collection c;
+	Collection c;  
+	
+}
+
+public static void main(String[] args) {
 	
 }
 }
